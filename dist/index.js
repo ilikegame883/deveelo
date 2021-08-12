07411f54c42c146f3309e824637746f1b0451783
Trei ☕ <65512990+Treixatek@users.noreply.github.com>
2021-08-11 23:40:37 -0400

    Convert to apollo-server-express

--- dist/index.js
+++ dist/index.js
@@ -3,34 +3,52 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const apollo_server_1 = require("apollo-server");
+require("dotenv/config");
+const apollo_server_express_1 = require("apollo-server-express");
 const graphql_middleware_1 = require("graphql-middleware");
 const schema_1 = require("@graphql-tools/schema");
 const mongoose_1 = __importDefault(require("mongoose"));
-require("dotenv/config");
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const typeDefs_1 = require("./graphql/typeDefs");
 const resolvers_1 = __importDefault(require("./graphql/resolvers"));
 const middleware_1 = require("./graphql/middleware");
-const schema = schema_1.makeExecutableSchema({
-    typeDefs: typeDefs_1.typeDefs,
-    resolvers: resolvers_1.default,
-});
-const middleware = [...middleware_1.authMiddlewares];
-const schemaWithMiddleware = graphql_middleware_1.applyMiddleware(schema, ...middleware);
-const server = new apollo_server_1.ApolloServer({
-    schema: schemaWithMiddleware,
-    context: ({ req, res }) => ({ req, res }),
-});
-mongoose_1.default
-    .connect(process.env.MONGODB_KEY, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-    .then(() => {
-    console.log("database connected");
-    return server.listen({ port: 5000 });
-})
-    .then((res) => {
-    console.log(`Server running at ${res.url} 🎉`);
+const initServer = async () => {
+    const app = express_1.default();
+    app.use(cors_1.default({
+        origin: "https://studio.apollographql.com",
+        credentials: true,
+    }));
+    app.get("/", (_req, res) => res.send("hello"));
+    app.post("/", (_req, res) => res.send("post test"));
+    const schema = schema_1.makeExecutableSchema({
+        typeDefs: typeDefs_1.typeDefs,
+        resolvers: resolvers_1.default,
+    });
+    const middleware = [...middleware_1.authMiddlewares];
+    const schemaWithMiddleware = graphql_middleware_1.applyMiddleware(schema, ...middleware);
+    const server = new apollo_server_express_1.ApolloServer({
+        schema: schemaWithMiddleware,
+        context: ({ req, res }) => ({ req, res }),
+    });
+    await server.start();
+    server.applyMiddleware({
+        app,
+        cors: false,
+    });
+    mongoose_1.default
+        .connect(process.env.MONGODB_KEY, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+        .then(() => {
+        console.log("📬 - database connected");
+        return app.listen(parseInt(process.env.PORT), () => {
+            console.log(`🚀 - Server running at http://localhost:4000`);
+        });
+    });
+};
+initServer().catch((err) => {
+    console.log(err);
 });
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,62 @@
-import { ApolloServer } from "apollo-server";
-import { applyMiddleware } from "graphql-middleware";
+import "dotenv/config";
+import { ApolloServer } from "apollo-server-express";
+import { applyMiddleware as applyGqlMiddle } from "graphql-middleware";
 import { makeExecutableSchema } from "@graphql-tools/schema";
 import mongoose from "mongoose";
-import "dotenv/config";
+import express from "express";
+import cors from "cors";
 
 import { typeDefs } from "./graphql/typeDefs";
 import resolvers from "./graphql/resolvers";
 import { authMiddlewares } from "./graphql/middleware";
 
-// development  change mongodb user password & access
+// development  change mongodb user password & access - cors origin to site domain
+const initServer = async () => {
+	const app = express();
 
-const schema = makeExecutableSchema({
-	typeDefs,
-	resolvers,
-});
+	app.use(
+		cors({
+			origin: "https://studio.apollographql.com", //http://localhost:3000
+			credentials: true,
+		})
+	);
+	app.get("/", (_req, res) => res.send("hello"));
+	app.post("/", (_req, res) => res.send("post test"));
 
-const middleware = [...authMiddlewares];
-
-const schemaWithMiddleware = applyMiddleware(schema, ...middleware);
-
-const server = new ApolloServer({
-	schema: schemaWithMiddleware,
-	context: ({ req, res }) => ({ req, res }),
-});
-
-//connect to the mongodb database
-mongoose
-	.connect(process.env.MONGODB_KEY!, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	})
-	.then(() => {
-		console.log("database connected");
-		return server.listen({ port: 5000 });
-	})
-	.then((res) => {
-		console.log(`Server running at ${res.url} 🎉`);
+	const schema = makeExecutableSchema({
+		typeDefs,
+		resolvers,
 	});
+
+	const middleware = [...authMiddlewares];
+	const schemaWithMiddleware = applyGqlMiddle(schema, ...middleware);
+
+	const server = new ApolloServer({
+		schema: schemaWithMiddleware,
+		context: ({ req, res }) => ({ req, res }),
+	});
+
+	await server.start();
+
+	server.applyMiddleware({
+		app,
+		cors: false, // development  set cors paths
+	});
+
+	//connect to the mongodb database
+	mongoose
+		.connect(process.env.MONGODB_KEY!, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		})
+		.then(() => {
+			console.log("📬 - database connected");
+			return app.listen(parseInt(process.env.PORT!), () => {
+				console.log(`🚀 - Server running at http://localhost:4000`);
+			});
+		});
+};
+
+initServer().catch((err) => {
+	console.log(err);
+});
