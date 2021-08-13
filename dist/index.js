09a1834d688022aeb2c3c8802a6fe897389a19f6
Trei ☕ <65512990+Treixatek@users.noreply.github.com>
2021-08-12 21:43:49 -0400

    Refresh access token on expiration

--- dist/index.js
+++ dist/index.js
@@ -7,20 +7,43 @@ require("dotenv/config");
 const apollo_server_express_1 = require("apollo-server-express");
 const graphql_middleware_1 = require("graphql-middleware");
 const schema_1 = require("@graphql-tools/schema");
+const jsonwebtoken_1 = require("jsonwebtoken");
 const mongoose_1 = __importDefault(require("mongoose"));
 const express_1 = __importDefault(require("express"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const cors_1 = __importDefault(require("cors"));
 const typeDefs_1 = require("./graphql/typeDefs");
 const resolvers_1 = __importDefault(require("./graphql/resolvers"));
 const middleware_1 = require("./graphql/middleware");
+const User_1 = __importDefault(require("./models/User"));
+const auth_1 = require("./util/auth");
 const initServer = async () => {
     const app = express_1.default();
     app.use(cors_1.default({
         origin: "https://studio.apollographql.com",
         credentials: true,
     }));
+    app.use(cookie_parser_1.default());
     app.get("/", (_req, res) => res.send("hello"));
-    app.post("/", (_req, res) => res.send("post test"));
+    app.post("/refresh_token", async (req, res) => {
+        const token = req.cookies.lid;
+        if (!token) {
+            return res.send({ ok: false, accessToken: "" });
+        }
+        let payload = null;
+        try {
+            payload = jsonwebtoken_1.verify(token, process.env.REFRESH_TOEKEN_SECRET);
+        }
+        catch (error) {
+            console.log(error);
+            return res.send({ ok: false, accessToken: "" });
+        }
+        const user = await User_1.default.findById(payload.id);
+        if (!user) {
+            return res.send({ ok: false, accessToken: "" });
+        }
+        return res.send({ ok: true, accessToken: auth_1.createAccessToken(user) });
+    });
     const schema = schema_1.makeExecutableSchema({
         typeDefs: typeDefs_1.typeDefs,
         resolvers: resolvers_1.default,
--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,17 @@ import "dotenv/config";
 import { ApolloServer } from "apollo-server-express";
 import { applyMiddleware as applyGqlMiddle } from "graphql-middleware";
 import { makeExecutableSchema } from "@graphql-tools/schema";
+import { verify } from "jsonwebtoken";
 import mongoose from "mongoose";
 import express from "express";
+import cookieParser from "cookie-parser";
 import cors from "cors";
 
 import { typeDefs } from "./graphql/typeDefs";
 import resolvers from "./graphql/resolvers";
 import { authMiddlewares } from "./graphql/middleware";
+import User, { UserType } from "./models/User";
+import { createAccessToken } from "./util/auth";
 
 // development  change mongodb user password & access - cors origin to site domain
 const initServer = async () => {
@@ -20,8 +24,35 @@ const initServer = async () => {
 			credentials: true,
 		})
 	);
+	//api routes
+	app.use(cookieParser());
 	app.get("/", (_req, res) => res.send("hello"));
-	app.post("/", (_req, res) => res.send("post test"));
+	app.post("/refresh_token", async (req, res) => {
+		//check if refresh token is correct & send new access token
+		const token = req.cookies.lid;
+		if (!token) {
+			//they are not signed in
+			return res.send({ ok: false, accessToken: "" });
+		}
+
+		let payload: any = null;
+		try {
+			payload = verify(token, process.env.REFRESH_TOEKEN_SECRET!);
+		} catch (error) {
+			console.log(error);
+			return res.send({ ok: false, accessToken: "" });
+		}
+
+		//token is valid, send access token
+		const user: UserType = await User.findById(payload.id);
+
+		if (!user) {
+			return res.send({ ok: false, accessToken: "" });
+		}
+
+		//login the user
+		return res.send({ ok: true, accessToken: createAccessToken(user) });
+	});
 
 	const schema = makeExecutableSchema({
 		typeDefs,
