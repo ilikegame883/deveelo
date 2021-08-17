2a2a546d3a6cfd685053dc7509936ce71fb88f6a
Trei ☕ <65512990+Treixatek@users.noreply.github.com>
2021-08-16 20:32:01 -0400

    Component blacklists based on routes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from "next/app";
 import { useApollo } from "../lib/apolloClient";
 import { ApolloProvider } from "@apollo/client";
+import { useRouter } from "next/router";
 
 import Layout from "../components/Layout";
 import "../styles/globals.css";
@@ -8,13 +9,34 @@ import "../styles/globals.css";
 function MyApp({ Component, pageProps }: AppProps) {
 	const apolloClient = useApollo(pageProps.initialApolloState);
 
+	const router = useRouter();
+	const currPage = router.pathname;
+
 	return (
 		<ApolloProvider client={apolloClient}>
-			<Layout showNav={true} showSidebar={true} showActivityBar={true}>
+			<Layout showNav={OnNavBlacklist(currPage)} showSidebar={OnSidebarBlacklist(currPage)} showActivityBar={OnActivityBlacklist(currPage)}>
 				<Component {...pageProps} />
 			</Layout>
 		</ApolloProvider>
 	);
 }
+
+const OnSidebarBlacklist = (route: string): boolean => {
+	const blacklist = ["/login", "/register"];
+
+	return !blacklist.includes(route);
+};
+
+const OnActivityBlacklist = (route: string): boolean => {
+	const blacklist = ["/login", "/register"];
+
+	return !blacklist.includes(route);
+};
+
+const OnNavBlacklist = (route: string): boolean => {
+	const blacklist = ["/login", "/register"];
+
+	return !blacklist.includes(route);
+};
 
 export default MyApp;
