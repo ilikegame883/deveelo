782a60fec3dc01caa973777ef20b54dd1ffb475a
Trei ☕ <65512990+Treixatek@users.noreply.github.com>
2021-09-08 21:50:36 -0400

    Form input syling

--- eros/components/minor/Form.tsx
+++ eros/components/minor/Form.tsx
@@ -4,14 +4,14 @@ const Form = () => {
 	return (
 		<form className={formStyles.largeContainer}>
 			<div className={formStyles.field}>
-				<input className={formStyles.input} type="emailorname" name="emailorname" placeholder="" />
+				<input className={formStyles.input} type="emailorname" name="emailorname" placeholder=" " />
 				<label htmlFor="emailorname" className={formStyles.label}>
 					Enter email or username
 				</label>
 			</div>
 
 			<div className={formStyles.field}>
-				<input className={formStyles.input} type="password" name="password" placeholder="" />
+				<input className={formStyles.input} type="password" name="password" placeholder=" " />
 				<label htmlFor="password" className={formStyles.label}>
 					Enter password
 				</label>
