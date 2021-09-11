b1e8159fa2d191774022d7a84a83c5b921ab3295
Trei ☕ <65512990+Treixatek@users.noreply.github.com>
2021-09-10 22:50:23 -0400

    Recording form input

--- eros/components/minor/Form.tsx
+++ eros/components/minor/Form.tsx
@@ -1,21 +1,51 @@
+import { useState } from "react";
+
 import formStyles from "../../styles/form.module.css";
 
 const Form = () => {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
 	return (
-		<form className={formStyles.largeContainer}>
+		<form
+			className={formStyles.largeContainer}
+			onSubmit={(e) => {
+				e.preventDefault();
+				console.log("form submitted");
+				console.log(email, password);
+			}}>
 			<div className={formStyles.field}>
-				<input className={formStyles.input} type="emailorname" name="emailorname" placeholder=" " />
+				<input
+					className={formStyles.input}
+					value={email}
+					type="emailorname"
+					name="emailorname"
+					placeholder=" "
+					onChange={(e) => {
+						setEmail(e.target.value);
+					}}
+				/>
 				<label htmlFor="emailorname" className={formStyles.label}>
 					Enter email or username
 				</label>
 			</div>
 
 			<div className={formStyles.field}>
-				<input className={formStyles.input} type="password" name="password" placeholder=" " />
+				<input
+					className={formStyles.input}
+					value={password}
+					type="password"
+					name="password"
+					placeholder=" "
+					onChange={(e) => {
+						setPassword(e.target.value);
+					}}
+				/>
 				<label htmlFor="password" className={formStyles.label}>
 					Enter password
 				</label>
 			</div>
+			<button type="submit">Login</button>
 		</form>
 	);
 };
