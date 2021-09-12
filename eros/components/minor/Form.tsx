a4c3cfb841f180c3b8d0f9549084082ec3e0dd70
Trei ☕ <65512990+Treixatek@users.noreply.github.com>
2021-09-12 19:18:36 -0400

    Show password toggle

--- eros/components/minor/Form.tsx
+++ eros/components/minor/Form.tsx
@@ -1,10 +1,17 @@
 import { useState } from "react";
+import Image from "next/image";
 
 import formStyles from "../../styles/form.module.css";
 
 const Form = () => {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
+	let showPassword = false;
+	const [show, setShow] = useState(false);
+
+	const toggleClass = () => {
+		setShow(!show);
+	};
 
 	return (
 		<form
@@ -18,7 +25,7 @@ const Form = () => {
 				<input
 					className={formStyles.input}
 					value={email}
-					type="emailorname"
+					type="text"
 					name="emailorname"
 					placeholder=" "
 					onChange={(e) => {
@@ -34,7 +41,7 @@ const Form = () => {
 				<input
 					className={formStyles.input}
 					value={password}
-					type="password"
+					type={show ? "text" : "password"}
 					name="password"
 					placeholder=" "
 					onChange={(e) => {
@@ -44,6 +51,10 @@ const Form = () => {
 				<label htmlFor="password" className={formStyles.label}>
 					Enter password
 				</label>
+
+				<span className={show ? formStyles.togglePassword : formStyles.toggledPassword} onMouseDown={() => toggleClass()}>
+					<Image src={show ? "/resources/eyeHide.svg" : "/resources/eye.svg"} width="22.5" height="22.5" />
+				</span>
 			</div>
 			<button type="submit">Login</button>
 		</form>
