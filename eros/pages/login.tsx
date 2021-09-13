b18f152de665ed2329f41e55f10866bc019f36e6
Trei ☕ <65512990+Treixatek@users.noreply.github.com>
2021-09-12 20:55:34 -0400

    Change login page header

--- eros/pages/login.tsx
+++ eros/pages/login.tsx
@@ -6,7 +6,7 @@ function login() {
 	return (
 		<div className={formStyles.largeContainerOffset}>
 			<p className={headerStyles.aboveHeader}>Hello,</p>
-			<h1 className={headerStyles.headerLarge}>Welcome to Deveelo</h1>
+			<h1 className={headerStyles.headerLarge}>Welcome back</h1>
 			<Form />
 		</div>
 	);
