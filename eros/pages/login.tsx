1c713eb404024f54beaaeaddc528f10225d150d4
Trei ☕ <65512990+Treixatek@users.noreply.github.com>
2021-12-30 20:44:59 -0500

    Redirect logged in users from login/reg pages

--- eros/pages/login.tsx
+++ eros/pages/login.tsx
@@ -1,8 +1,16 @@
-import Form from "../components/minor/Form";
+import router from "next/router";
 import formStyles from "../styles/form.module.css";
 import headerStyles from "../styles/headers.module.css";
 
+import Form from "../components/minor/Form";
+import { getAccessToken } from "../accessToken";
+
 function login() {
+	const accessToken = getAccessToken();
+	if (accessToken && accessToken !== "") {
+		router.push("/");
+	}
+
 	return (
 		<div className={formStyles.largeContainerOffset}>
 			<p className={headerStyles.aboveHeader}>Hello,</p>
