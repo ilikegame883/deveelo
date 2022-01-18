69aa398f009732c3eeddcdf41b7b293aee3172a6
Trei ☕ <65512990+Treixatek@users.noreply.github.com>
2022-01-17 22:27:43 -0500

    Nonblurry app icon

--- pavo/main.js
+++ pavo/main.js
@@ -1,5 +1,7 @@
 const { app, BrowserWindow } = require("electron");
 if (require("electron-squirrel-startup")) app.quit();
+const path = require("path");
+
 require("update-electron-app")();
 
 const createWindow = () => {
@@ -8,6 +10,7 @@ const createWindow = () => {
 		height: 800,
 		frame: false,
 		show: true,
+		icon: process.platform === "win32" ? path.join(__dirname, "build/icon.ico") : path.join(__dirname, "build/icon.icns"),
 	});
 	win.loadURL("http://localhost:3000");
 	win.removeMenu();
