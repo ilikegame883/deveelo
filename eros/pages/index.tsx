5192c5656e8e5e19dafb015fdc9df61fabc79da5
Trei ☕ <65512990+Treixatek@users.noreply.github.com>
2022-01-20 20:18:06 -0500

    Update meta w/ tab notifications

--- eros/pages/index.tsx
+++ eros/pages/index.tsx
@@ -1,9 +1,21 @@
 import Meta from "../components/micro/Meta";
 
+let notifEmojis = new Map<string, string>([
+	["gift", "🎁"],
+	["update", "🎀"],
+	["message", "🔥"],
+	["friend", "💝"],
+	["views", "🔴"],
+]);
+
 export default function Home() {
+	const notifs: string[] = ["message", "message", "gift", "message", "message", "friend"];
+
+	const title = notifs.length > 0 ? `Deveelo | ${notifEmojis.get(notifs[0]) + notifs.length}` : "Deveelo";
+
 	return (
 		<div>
-			<Meta title="Deveelo"></Meta>
+			<Meta title={title}></Meta>
 		</div>
 	);
 }
