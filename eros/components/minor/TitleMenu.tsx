257d0cbbf307958fb8ae8e205cc9a457302cb4c5
Trei ☕ <65512990+Treixatek@users.noreply.github.com>
2022-01-27 20:32:55 -0500

    Instantly bind titlebar buttons

--- eros/components/minor/TitleMenu.tsx
+++ eros/components/minor/TitleMenu.tsx
@@ -1,17 +1,11 @@
-import { appWindow } from "@tauri-apps/api/window";
-import { useEffect, useState } from "react";
+import bindTitlebar from "../../hooks/bindTitlebar";
 import getVersion from "../../hooks/getVersion";
 import isLuna from "../../hooks/isLuna";
 import navStyles from "../../styles/nav.module.css";
 
 const TitleMenu = () => {
 	if (isLuna()) {
-		document.getElementById("titlebar-minimize")?.addEventListener("click", () => appWindow.minimize());
-		document.getElementById("titlebar-maximize")?.addEventListener("click", async () => {
-			let max = await appWindow.isMaximized();
-			max ? appWindow.unmaximize() : appWindow.maximize();
-		});
-		document.getElementById("titlebar-close")?.addEventListener("click", () => appWindow.close());
+		bindTitlebar();
 	}
 
 	return (
