92b89c885b158b1dabbc642bc777f3b817b626f6
Trei ☕ <65512990+Treixatek@users.noreply.github.com>
2022-02-01 23:07:22 -0500

    Add event handling to all TextButtons

--- eros/components/micro/TextButton.tsx
+++ eros/components/micro/TextButton.tsx
@@ -29,14 +29,14 @@ const TextButton = ({ colorKey, text, submit, action, disabled }: buttonParams) 
 			break;
 		case "green":
 			content = (
-				<button className={buttonStyles.greenGrad} type={submit ? "submit" : undefined}>
+				<button className={buttonStyles.greenGrad} type={submit ? "submit" : undefined} onClick={(e) => handlePress()}>
 					{text}
 				</button>
 			);
 			break;
 		default:
 			content = (
-				<button className={buttonStyles.goldGrad} type={submit ? "submit" : undefined}>
+				<button className={buttonStyles.goldGrad} type={submit ? "submit" : undefined} onClick={(e) => handlePress()}>
 					{text}
 				</button>
 			);
