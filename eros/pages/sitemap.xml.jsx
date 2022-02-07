d64db74f731f157d3b76a9ffbd58272891067215
Trei ☕ <65512990+Treixatek@users.noreply.github.com>
2022-02-06 21:21:29 -0500

    Testing search route

--- eros/pages/sitemap.xml.jsx
+++ eros/pages/sitemap.xml.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 //https://vega-deployment.herokuapp.com/users
-const EXTERNAL_DATA_URL = "https://vega-deployment.herokuapp.com:4000/users";
+// const EXTERNAL_DATA_URL = "http://localhost:4000/users";
+const EXTERNAL_DATA_URL = "https://vega-deployment.herokuapp.com/search?name=batu";
 
 const createSitemap = (users) => `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
@@ -22,7 +23,7 @@ const createSitemap = (users) => `<?xml version="1.0" encoding="UTF-8"?>
 class Sitemap extends React.Component {
 	static async getInitialProps({ res }) {
 		//get users
-		console.log("start of sitemap");
+		console.log("start of sitemap fetch from " + EXTERNAL_DATA_URL);
 		const request = await fetch(EXTERNAL_DATA_URL, {
 			method: "GET",
 			credentials: "include",
