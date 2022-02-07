081d6005221dd689e72802d8cfc347f782deaf28
Trei ☕ <65512990+Treixatek@users.noreply.github.com>
2022-02-06 21:41:57 -0500

    Fix dummy data

--- eros/pages/sitemap.xml.jsx
+++ eros/pages/sitemap.xml.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 //https://vega-deployment.herokuapp.com/users
 // const EXTERNAL_DATA_URL = "http://localhost:4000/users";
-const EXTERNAL_DATA_URL = "https://jsonplaceholder.typicode.com/posts/1";
+const EXTERNAL_DATA_URL = "https://jsonplaceholder.typicode.com/posts";
 
 const createSitemap = (posts) => `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
@@ -25,7 +25,7 @@ class Sitemap extends React.Component {
 		//get users
 		console.log("start of sitemap fetch from " + EXTERNAL_DATA_URL);
 		const request = await fetch(EXTERNAL_DATA_URL, {
-			method: "POST",
+			method: "GET",
 			credentials: "include",
 			mode: "cors",
 		});
