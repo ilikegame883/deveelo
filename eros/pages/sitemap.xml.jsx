b5457f7f698ec820aedd5e0f53956ea2f71815a0
Trei ☕ <65512990+Treixatek@users.noreply.github.com>
2022-02-06 21:24:27 -0500

    Test refreshtoken

--- eros/pages/sitemap.xml.jsx
+++ eros/pages/sitemap.xml.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 //https://vega-deployment.herokuapp.com/users
 // const EXTERNAL_DATA_URL = "http://localhost:4000/users";
-const EXTERNAL_DATA_URL = "https://vega-deployment.herokuapp.com/search?name=batu";
+const EXTERNAL_DATA_URL = "https://vega-deployment.herokuapp.com/refresh_token";
 
 const createSitemap = (users) => `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
@@ -25,7 +25,7 @@ class Sitemap extends React.Component {
 		//get users
 		console.log("start of sitemap fetch from " + EXTERNAL_DATA_URL);
 		const request = await fetch(EXTERNAL_DATA_URL, {
-			method: "GET",
+			method: "POST",
 			credentials: "include",
 			mode: "cors",
 		});
