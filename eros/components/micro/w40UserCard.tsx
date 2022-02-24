522ef9c606913b527fe0ac4181fba0ca2e1dae83
Trei ☕ <65512990+Treixatek@users.noreply.github.com>
2022-02-24 14:08:09 -0500

    w40 card beginnings

---
+++ eros/components/micro/w40UserCard.tsx
@@ -0,0 +1,15 @@
+import { SearchUserType } from "../../lib/userTypes";
+import NameGroup from "./NameGroup";
+import ProfilePicture from "./ProfilePicture";
+
+const w40UserCard = ({ id }: { id: string }) => {
+	/* todo 
+    -  make findbyid query
+    -  call query from here
+    -  use returned user to generate card
+    -  finish .map & test
+    */
+	return <div>w40UserCard</div>;
+};
+
+export default w40UserCard;
