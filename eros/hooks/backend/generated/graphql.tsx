bea83b3390280598dc8259dab084a8ff410f2980
Trei ☕ <65512990+Treixatek@users.noreply.github.com>
2022-02-27 22:10:27 -0500

    Follow hooks

--- eros/hooks/backend/generated/graphql.tsx
+++ eros/hooks/backend/generated/graphql.tsx
@@ -30,6 +30,8 @@ export type Mutation = {
   register: LoginResponse;
   login: LoginResponse;
   logout: Scalars['Boolean'];
+  follow?: Maybe<BoolRes>;
+  unfollow?: Maybe<BoolRes>;
 };
 
 
@@ -42,6 +44,16 @@ export type MutationRegisterArgs = {
 export type MutationLoginArgs = {
   input: Scalars['String'];
   password: Scalars['String'];
+};
+
+
+export type MutationFollowArgs = {
+  id: Scalars['String'];
+};
+
+
+export type MutationUnfollowArgs = {
+  id: Scalars['String'];
 };
 
 export type Post = {
@@ -154,6 +166,13 @@ export type FindMinProfileByTagQueryVariables = Exact<{
 
 export type FindMinProfileByTagQuery = { __typename?: 'Query', findUserByTag: { __typename?: 'User', _id: string, status: string, account: { __typename?: 'U_Account', username: string, tag: string, private: boolean }, profile: { __typename?: 'U_Profile', bannerUrl: string, pictureUrl: string, description: string, followingIds: Array<Maybe<string>>, followerIds: Array<Maybe<string>>, badges: Array<Maybe<string>>, linkedProfiles: Array<Maybe<string>> }, social: { __typename?: 'U_Social', postIds: Array<Maybe<string>>, blogIds: Array<Maybe<string>> } } };
 
+export type Unnamed_1_MutationVariables = Exact<{
+  targetId: Scalars['String'];
+}>;
+
+
+export type Unnamed_1_Mutation = { __typename?: 'Mutation', follow?: Maybe<{ __typename?: 'BoolRes', success: boolean }> };
+
 export type LoginMutationVariables = Exact<{
   loginInput: Scalars['String'];
   loginPassword: Scalars['String'];
@@ -196,6 +215,13 @@ export type SampleUsersQueryVariables = Exact<{
 
 
 export type SampleUsersQuery = { __typename?: 'Query', randomUsers: Array<Maybe<{ __typename?: 'User', status: string, account: { __typename?: 'U_Account', username: string, tag: string }, profile: { __typename?: 'U_Profile', pictureUrl: string } }>> };
+
+export type Unnamed_2_MutationVariables = Exact<{
+  targetId: Scalars['String'];
+}>;
+
+
+export type Unnamed_2_Mutation = { __typename?: 'Mutation', unfollow?: Maybe<{ __typename?: 'BoolRes', success: boolean }> };
 
 
 export const GetPostsDocument = gql`
@@ -369,6 +395,39 @@ export function useFindMinProfileByTagLazyQuery(baseOptions?: Apollo.LazyQueryHo
 export type FindMinProfileByTagQueryHookResult = ReturnType<typeof useFindMinProfileByTagQuery>;
 export type FindMinProfileByTagLazyQueryHookResult = ReturnType<typeof useFindMinProfileByTagLazyQuery>;
 export type FindMinProfileByTagQueryResult = Apollo.QueryResult<FindMinProfileByTagQuery, FindMinProfileByTagQueryVariables>;
+export const Document = gql`
+    mutation ($targetId: String!) {
+  follow(id: $targetId) {
+    success
+  }
+}
+    `;
+export type MutationFn = Apollo.MutationFunction<Mutation, MutationVariables>;
+
+/**
+ * __useMutation__
+ *
+ * To run a mutation, you first call `useMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [mutation, { data, loading, error }] = useMutation({
+ *   variables: {
+ *      targetId: // value for 'targetId'
+ *   },
+ * });
+ */
+export function useMutation(baseOptions?: Apollo.MutationHookOptions<Mutation, MutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<Mutation, MutationVariables>(Document, options);
+      }
+export type MutationHookResult = ReturnType<typeof useMutation>;
+export type MutationResult = Apollo.MutationResult<Mutation>;
+export type MutationOptions = Apollo.BaseMutationOptions<Mutation, MutationVariables>;
 export const LoginDocument = gql`
     mutation Login($loginInput: String!, $loginPassword: String!) {
   login(input: $loginInput, password: $loginPassword) {
@@ -687,3 +746,36 @@ export function useSampleUsersLazyQuery(baseOptions?: Apollo.LazyQueryHookOption
 export type SampleUsersQueryHookResult = ReturnType<typeof useSampleUsersQuery>;
 export type SampleUsersLazyQueryHookResult = ReturnType<typeof useSampleUsersLazyQuery>;
 export type SampleUsersQueryResult = Apollo.QueryResult<SampleUsersQuery, SampleUsersQueryVariables>;
+export const Document = gql`
+    mutation ($targetId: String!) {
+  unfollow(id: $targetId) {
+    success
+  }
+}
+    `;
+export type MutationFn = Apollo.MutationFunction<Mutation, MutationVariables>;
+
+/**
+ * __useMutation__
+ *
+ * To run a mutation, you first call `useMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [mutation, { data, loading, error }] = useMutation({
+ *   variables: {
+ *      targetId: // value for 'targetId'
+ *   },
+ * });
+ */
+export function useMutation(baseOptions?: Apollo.MutationHookOptions<Mutation, MutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<Mutation, MutationVariables>(Document, options);
+      }
+export type MutationHookResult = ReturnType<typeof useMutation>;
+export type MutationResult = Apollo.MutationResult<Mutation>;
+export type MutationOptions = Apollo.BaseMutationOptions<Mutation, MutationVariables>;
