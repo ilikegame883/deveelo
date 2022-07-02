9bf5227430609ba17b52b7a1f44b13f938a14b50
Trei ☕ <65512990+Treixatek@users.noreply.github.com>
2022-07-01 20:07:56 -0400

    Comment titlebar functions

--- eros/hooks/bindTitlebar.ts
+++ eros/hooks/bindTitlebar.ts
@@ -1,9 +1,12 @@
 import { appWindow } from "@tauri-apps/api/window";
 
 const bindTitlebar = async () => {
+	//wait 100ms (so after the buttons have been created), then bind
+	//the window management functions too them
 	setTimeout(() => {
 		document.getElementById("titlebar-minimize")?.addEventListener("click", () => appWindow.minimize());
 		document.getElementById("titlebar-maximize")?.addEventListener("click", async () => {
+			//manually max & min, the builtin tauri function does not work properly
 			let max = await appWindow.isMaximized();
 			max ? appWindow.unmaximize() : appWindow.maximize();
 		});
