ccaca72a4f0e288bc3f620ba836a15f2f3f2cbde
Trei ☕ <65512990+Treixatek@users.noreply.github.com>
2022-07-02 16:43:02 -0400

    Move unfollow hook into a component:--The sidebar

--- eros/hooks/socialhooks.ts
+++ eros/hooks/socialhooks.ts
@@ -1,5 +1,3 @@
-import { useFollowMutation, useUnfollowMutation } from "./backend/generated/graphql";
-
 export const updateSidebar = (tag: string) => {
 	const side = document.getElementById("sidebar");
 	if (side) {
@@ -9,61 +7,3 @@ export const updateSidebar = (tag: string) => {
 		side.dispatchEvent(updateEvent);
 	}
 };
-
-// export const followUser = async (id: string, target: string) => {
-// 	const [followUser] = useFollowMutation();
-
-// 	try {
-// 		console.log("ran");
-
-// 		const response = await followUser({
-// 			variables: {
-// 				targetId: id,
-// 			},
-// 		});
-
-// 		if (response && response.data) {
-// 			if (response.data.follow.success === true) {
-// 				switch (target) {
-// 					case "sidebar":
-// 						updateSidebar(null);
-// 						break;
-// 					default:
-// 						break;
-// 				}
-// 			}
-// 		}
-// 	} catch (error) {
-// 		console.log(error);
-// 	}
-
-// 	return;
-// };
-
-// export const unfollowUser = async (id: string, target: string) => {
-// 	const [unfollowUser] = useUnfollowMutation();
-
-// 	try {
-// 		const response = await unfollowUser({
-// 			variables: {
-// 				targetId: id,
-// 			},
-// 		});
-
-// 		if (response && response.data) {
-// 			if (response.data.unfollow.success === true) {
-// 				switch (target) {
-// 					case "sidebar":
-// 						updateSidebar(null);
-// 						break;
-// 					default:
-// 						break;
-// 				}
-// 			}
-// 		}
-// 	} catch (error) {
-// 		console.log(error);
-// 	}
-
-// 	return;
-// };
