769dd440394ed726a2ba4fe5e20f059131d142b3
Trei ☕ <65512990+Treixatek@users.noreply.github.com>
2022-07-04 12:09:13 -0400

    Remove lastfmod in localstore and document absfmod

--- eros/hooks/setSidebar.ts
+++ eros/hooks/setSidebar.ts
@@ -2,17 +2,11 @@ export const setSideBarByTag = (tag: string): void => {
 	window.localStorage.setItem("side_prof", tag);
 
 	//This is used to keep track of the modifications to the display follower count
-	//Serves as a way to know the last modification, will be used by sidebar
-	//will store last 2 actions: lastfmod = +|batu~-|batu which is ["+", "-"] (- is most recent)
-	window.localStorage.setItem("lastfmod", "");
+	//Stores the absolute change that has occured to the original value. i.e. we follow
+	//and the fmod increases to 1 (this can be added to the count display), then we unfollow
+	//and it goes back to 0 where it is reset to "". If we unfollow first, value goes to -1 -> 0
 	window.localStorage.setItem("fmod", "");
 };
-
-export interface FmodFetch {
-	notset: boolean;
-	direction: string;
-	tag: string;
-}
 
 export interface Fmod {
 	set: boolean;
