9cfab7f1550354f69b3c970d196cdcc6a4c8c23d
Ashton <65512990+Treixatek@users.noreply.github.com>
2022-07-20 21:50:10 -0400

    Custom payload conscious GQL resolver typ

--- vega/src/util/middlewareType.ts
+++ vega/src/util/middlewareType.ts
@@ -1,13 +1,19 @@
 import { GraphQLResolveInfo } from "graphql";
-import { StitchingInfo } from "@graphql-tools/delegate";
+//import { StitchingInfo } from "@graphql-tools/delegate";
 
 import Context from "src/context";
 
 //slightly different version of gql-middleware resolver type. This sets the ctx to our custom Context.ts
-export declare type NewIMiddlewareResolver<TSource = any, TContext = Context, TArgs = any> = (
-	resolve: (source?: TSource, args?: TArgs, context?: TContext, info?: GraphQLResolveInfo & { stitchingInfo?: StitchingInfo }) => any,
-	parent: TSource,
-	args: TArgs,
-	context: TContext,
-	info: GraphQLResolveInfo
-) => Promise<any>;
+// export declare type NewIMiddlewareResolver<TSource = any, TContext = Context, TArgs = any> = (
+// 	resolve: (source?: TSource, args?: TArgs, context?: TContext, info?: GraphQLResolveInfo & { stitchingInfo?: StitchingInfo }) => any,
+// 	parent: TSource,
+// 	args: TArgs,
+// 	context: TContext,
+// 	info: GraphQLResolveInfo
+// ) => Promise<any>;
+
+export declare type MyGraphQLFieldResolver<TSource, TContext = Context, TArgs = any, TResult = unknown> = (source: TSource, args: TArgs, context: TContext, info: GraphQLResolveInfo) => TResult;
+
+//custom middleware type from the gql-tools/resolver-comp, uses the custom gqlFieldResolver above
+//I also make the parameters not any, the last two at least
+export declare type MyResolversComposition<Resolver extends MyGraphQLFieldResolver<any, any, any> = MyGraphQLFieldResolver<any>> = (next: Resolver) => Resolver;
