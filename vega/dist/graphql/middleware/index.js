4d69b87f9ab9a9c5b59cac621815f02a3232e84e
Ashton <65512990+Treixatek@users.noreply.github.com>
2022-07-22 17:21:08 -0400

    Explicit resolver assingment for composition

--- vega/dist/graphql/middleware/index.js
+++ vega/dist/graphql/middleware/index.js
@@ -3,4 +3,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.authMiddlewares = void 0;
 const isAuth_1 = require("./isAuth");
 exports.authMiddlewares = [isAuth_1.isAuth];
+const resolversComposition = {
+    Query: {
+        myAccount: [isAuth_1.loggedInOnlyAuth()],
+    },
+    Mutation: {
+        logout: [isAuth_1.loggedInOnlyAuth()],
+        follow: [isAuth_1.loggedInOnlyAuth()],
+        unfollow: [isAuth_1.loggedInOnlyAuth()],
+        updateProfile: [isAuth_1.loggedInOnlyAuth()],
+    },
+};
 //# sourceMappingURL=index.js.map
--- vega/src/graphql/middleware/index.ts
+++ vega/src/graphql/middleware/index.ts
@@ -1,3 +1,43 @@
-import { isAuth } from "./isAuth";
+import { ResolversComposition, ResolversComposerMapping } from "@graphql-tools/resolvers-composition";
+import resolvers from "../resolvers";
+
+import { isAuth, loggedInOnlyAuth } from "./isAuth";
 
 export const authMiddlewares = [isAuth]; //middle, where auth middleware is compiled
+
+//assign middleware to resolvers
+// const resolversComposition: ResolversComposerMapping<typeof resolvers> = {
+//     Query: {
+// 		myAccount: [loggedInOnlyAuth()],
+// 	},
+// 	// Mutation: {
+// 	// 	logout: loggedInOnlyAuth(),
+// 	// 	follow: loggedInOnlyAuth(),
+// 	// 	unfollow: loggedInOnlyAuth(),
+// 	// 	updateProfile: loggedInOnlyAuth(),
+// 	// },
+// };
+
+interface MiddlewareEnabledResolvers {
+	Query: {
+		myAccount: any;
+	};
+	Mutation: {
+		logout: any;
+		follow: any;
+		unfollow: any;
+		updateProfile: any;
+	};
+}
+
+const resolversComposition: ResolversComposerMapping<MiddlewareEnabledResolvers> = {
+	Query: {
+		myAccount: [loggedInOnlyAuth()],
+	},
+	Mutation: {
+		logout: [loggedInOnlyAuth()],
+		follow: [loggedInOnlyAuth()],
+		unfollow: [loggedInOnlyAuth()],
+		updateProfile: [loggedInOnlyAuth()],
+	},
+};
