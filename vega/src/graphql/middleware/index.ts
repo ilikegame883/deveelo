55ca21d013b52771d1143a1b14c122b391a16ff5
Ashton <65512990+Treixatek@users.noreply.github.com>
2022-07-22 17:22:57 -0400

    Remove old type of assignment method

--- vega/src/graphql/middleware/index.ts
+++ vega/src/graphql/middleware/index.ts
@@ -5,19 +5,6 @@ import { isAuth, loggedInOnlyAuth } from "./isAuth";
 
 export const authMiddlewares = [isAuth]; //middle, where auth middleware is compiled
 
-//assign middleware to resolvers
-// const resolversComposition: ResolversComposerMapping<typeof resolvers> = {
-//     Query: {
-// 		myAccount: [loggedInOnlyAuth()],
-// 	},
-// 	// Mutation: {
-// 	// 	logout: loggedInOnlyAuth(),
-// 	// 	follow: loggedInOnlyAuth(),
-// 	// 	unfollow: loggedInOnlyAuth(),
-// 	// 	updateProfile: loggedInOnlyAuth(),
-// 	// },
-// };
-
 interface MiddlewareEnabledResolvers {
 	Query: {
 		myAccount: any;
