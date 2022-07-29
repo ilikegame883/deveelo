8a0914dcf62b4247573fdcbdde551821b32cb12f
Ashton <65512990+ashtonland@users.noreply.github.com>
2022-07-28 22:33:59 -0400

    Gobal front & backend nodenv based url hooks

---
+++ eros/lib/links.ts
@@ -0,0 +1,10 @@
+export const prodServerUrl = "https://vega-deployment.herokuapp.com/";
+
+export const devServerUrl = "http://localhost:4000/";
+
+//get the current vega url, and allow navigation to sub path
+export const getServerUrl = (path?: string): string => {
+	const route = path ? path : "";
+
+	return process.env.NODE_ENV === "production" ? prodServerUrl + route : devServerUrl + route;
+};
