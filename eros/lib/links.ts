7053d206b65cdf8adaaf10b47137dc163dd53193
Ashton <65512990+ashtonland@users.noreply.github.com>
2022-07-29 12:27:23 -0400

    Get web url frontend hook

--- eros/lib/links.ts
+++ eros/lib/links.ts
@@ -1,5 +1,5 @@
+/* VEGA */
 export const prodServerUrl = "https://vega-deployment.herokuapp.com/";
-
 export const devServerUrl = "http://localhost:4000/";
 
 //get the current vega url, and allow navigation to sub path
@@ -7,4 +7,14 @@ export const getServerUrl = (path?: string): string => {
 	const route = path ? path : "";
 
 	return process.env.NODE_ENV === "production" ? prodServerUrl + route : devServerUrl + route;
+};
+
+/* EROS */
+export const prodWebUrl = "https://www.deveelo.com/";
+export const devWebUrl = "http://localhost:3000/";
+
+export const getWebUrl = (path?: string): string => {
+	const route = path ? path : "";
+
+	return process.env.NODE_ENV === "production" ? prodWebUrl + route : devWebUrl + route;
 };
