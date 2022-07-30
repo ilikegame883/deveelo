e55412a28b3f3eafe05d4fb6ffad7476beb3d72b
Ashton <65512990+ashtonland@users.noreply.github.com>
2022-07-29 23:13:28 -0400

    Store default banner's authors & unsplash links

---
+++ vega/src/util/credits.ts
@@ -0,0 +1,19 @@
+//files for providing credits to the unsplash photos used
+export const defaultBanners = {
+	image_1: {
+		user: "Eberhard Grossgasteiger",
+		link: "https://unsplash.com/@eberhardgross?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+	},
+	image_2: {
+		user: "Daniel Leone",
+		link: "https://unsplash.com/@danielleone?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+	},
+	image_3: {
+		user: "David Marcu",
+		link: "https://unsplash.com/@davidmarcu?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+	},
+	image_4: {
+		user: "Jerry Zhang",
+		link: "https://unsplash.com/@z734923105?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText",
+	},
+};
