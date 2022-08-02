815fb4c00a5174f75fd0ca8eb8c2cb17afe16cf3
Ashton <65512990+ashtonland@users.noreply.github.com>
2022-08-02 16:24:02 -0400

    Change banner/pfp to new image upload w/o reload

--- eros/hooks/loaders.ts
+++ eros/hooks/loaders.ts
@@ -11,6 +11,10 @@ export const bannerLoader: ImageLoader = ({ src, width, quality }: ImageLoaderPr
 	if (isDefault) {
 		return `${getWebUrl()}/${src}?w=${width}&q=${quality || 75}`;
 	} else {
-		return `${getServerUrl("uploads")}${src}`;
+		//we append the time as a parameter in the link to break the cache since
+		//a new image has been made at the same url, but next will just load the old
+		//image instead of checking to find the newly changed image at that url
+		//by using a unique link every time, we break the cache and network fetch always
+		return `${getServerUrl("uploads")}${src}?` + new Date().getTime();
 	}
 };
