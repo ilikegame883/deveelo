7d5ce6f8d8e26758868cf7632007700aa3c58f9e
Ashton <65512990+ashtonland@users.noreply.github.com>
2022-08-01 20:17:19 -0400

    Check upload file size util

--- eros/hooks/inputUtils.ts
+++ eros/hooks/inputUtils.ts
@@ -1,3 +1,17 @@
 export const removeSpaces = (input: string): string => {
 	return input.replace(/\s+/g, "");
 };
+
+export const checkFileSize = (inputSize: number, limit: string): boolean => {
+	const presets = new Map<string, number>();
+	presets.set("2mb", 2000000); //2 million bytes
+	presets.set("5mb", 5000000); //for gifs - usually 2.6 to 3.7
+
+	if (presets.has(limit)) {
+		const maxBytes = presets.get(limit);
+
+		return inputSize <= maxBytes;
+	}
+
+	throw new Error(`file size limit: ${limit}, is not among the preset values in check file size`);
+};
