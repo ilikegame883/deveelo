d702a47743621db4f0630d0b9d26ab9b7b7425f1
Ashton <65512990+ashtonland@users.noreply.github.com>
2022-08-08 10:51:27 -0400

    Profile stats component

---
+++ eros/components/minor/ProfileStats.tsx
@@ -0,0 +1,23 @@
+import statStyles from "../../styles/minor/profilestats.module.css";
+
+interface StatsProps {
+	following: number;
+	followers: number;
+	posts: number;
+}
+
+const ProfileStats = ({ following, followers, posts }: StatsProps) => {
+	console.log(posts);
+
+	return (
+		<div className={statStyles.largeWrapper}>
+			<div className={statStyles.item}>
+				{/* sometimes postsids is undefined */}
+				<p className={statStyles.statistic}>{posts ? posts : 0}</p>
+				<p className={statStyles.label}>Posts</p>
+			</div>
+		</div>
+	);
+};
+
+export default ProfileStats;
