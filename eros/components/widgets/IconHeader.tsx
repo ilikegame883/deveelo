ec3056f6f2784c9065d7a32fb4f2b0b716990316
Ashton <65512990+ashtonland@users.noreply.github.com>
2022-08-10 12:59:47 -0400

    Map header type input to css class

--- eros/components/widgets/IconHeader.tsx
+++ eros/components/widgets/IconHeader.tsx
@@ -1,7 +1,19 @@
 import iconheaderStyles from "../../styles/micro/iconheader.module.css";
 
-const IconHeader = () => {
-	return <div className={iconheaderStyles.container}>IconHeader</div>;
+interface IconHeaderProps {
+	type: "widget";
+	src: string;
+}
+
+const IconHeader = ({ type, src }: IconHeaderProps) => {
+	const textstyles = new Map<string, any>();
+	textstyles.set("widget", iconheaderStyles.widget);
+
+	return (
+		<div className={iconheaderStyles.container}>
+			<h5 className={textstyles.get(type)}></h5>
+		</div>
+	);
 };
 
 export default IconHeader;
