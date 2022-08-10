596df84cf6bf340ceb054a2b244ed7f3886cf6b0
Ashton <65512990+ashtonland@users.noreply.github.com>
2022-08-10 13:03:20 -0400

    Add header to people widget

--- eros/components/widgets/People.tsx
+++ eros/components/widgets/People.tsx
@@ -1,7 +1,12 @@
 import widgetStyles from "../../styles/minor/widget.module.css";
+import IconHeader from "./IconHeader";
 
-const PeopleWidget = () => {
-	return <div className={widgetStyles.peoplewidget}>PeopleWidget</div>;
+const PeopleWidget = ({ count }: { count: string }) => {
+	return (
+		<div className={widgetStyles.peoplewidget}>
+			<IconHeader type="widget" src="resources/person.svg" />
+		</div>
+	);
 };
 
 export default PeopleWidget;
