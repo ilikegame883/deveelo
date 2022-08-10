41887f921669b39fd08a628049187e46960bc142
Ashton <65512990+ashtonland@users.noreply.github.com>
2022-08-10 12:50:42 -0400

    Icon header component

---
+++ eros/components/widgets/IconHeader.tsx
@@ -0,0 +1,7 @@
+import iconheaderStyles from "../../styles/micro/iconheader.module.css";
+
+const IconHeader = () => {
+	return <div className={iconheaderStyles.container}>IconHeader</div>;
+};
+
+export default IconHeader;
