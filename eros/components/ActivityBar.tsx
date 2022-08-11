e225584fe24e4d7bd119a2ac24870ed83769bd01
Ashton <65512990+ashtonland@users.noreply.github.com>
2022-08-10 20:47:48 -0400

    Remove old method of loading widgets

--- eros/components/ActivityBar.tsx
+++ eros/components/ActivityBar.tsx
@@ -10,10 +10,6 @@ const ActivityBar = ({ widgetKeys, hardEdge }: activityBarParams) => {
 	//map the input keys to the widget component
 	const widgets = new Map<string, any>();
 	widgets.set("people", <PeopleWidget count={4} />);
-	// const load = (key: string) => {
-	// 	// shorten teh syntax needed to check if the widget is included in the "to load" list
-	// 	return widgetKeys.includes(key);
-	// }
 
 	return <div className={hardEdge ? activityStyles.activityBar_full : activityStyles.activityBar}>{widgetKeys.map((key) => widgets.get(key))}</div>;
 };
