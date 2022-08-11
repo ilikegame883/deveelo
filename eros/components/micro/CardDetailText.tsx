f1bed9e675927e478436eb6bb5effb279caae587
Ashton <65512990+ashtonland@users.noreply.github.com>
2022-08-11 16:24:38 -0400

    Detail text & bold text spacing

--- eros/components/micro/CardDetailText.tsx
+++ eros/components/micro/CardDetailText.tsx
@@ -2,13 +2,13 @@ import cardStyles from "../../styles/micro/widgetcards.module.css";
 
 interface DetailProps {
 	text: string;
-	boldtext: string;
+	boldtext?: string;
 }
 
 const CardDetailText = ({ text, boldtext }: DetailProps) => {
 	return (
 		<p className={cardStyles.detail}>
-			{text} {boldtext ? <span style={{ fontWeight: 700 }}>boldtext</span> : null}
+			{text} {boldtext ? <span style={{ fontWeight: 700, marginLeft: ".25em" }}>{boldtext}</span> : null}
 		</p>
 	);
 };
