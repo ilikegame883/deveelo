cdc08cf76b0547a0a4dd95cbb58a00f6c9c85a6d
Ashton <65512990+ashtonland@users.noreply.github.com>
2022-08-10 20:43:10 -0400

    Modular icon conditional coloring

--- eros/components/widgets/IconHeader.tsx
+++ eros/components/widgets/IconHeader.tsx
@@ -3,18 +3,22 @@ import iconheaderStyles from "../../styles/micro/iconheader.module.css";
 interface IconHeaderProps {
 	type: "widget";
 	src: string;
+	color: "purple" | "pink-yellow" | "gold" | "red" | "pinky-red" | "blue" | "green";
 }
 
 const purple = { background: "linear-gradient(120.25deg, #7594FF 11.25%, #685CEF 88.64%)", boxShadow: "0px 0.375em 0.938em rgba(104, 92, 239, 0.3)" };
 
-const IconHeader = ({ type, src }: IconHeaderProps) => {
+const IconHeader = ({ type, src, color }: IconHeaderProps) => {
 	// map type to header text styling
 	const textStyles = new Map<string, any>();
 	textStyles.set("widget", iconheaderStyles.widget);
+	//map input to icon color
+	const iconColors = new Map<string, any>();
+	iconColors.set("purple", purple);
 
 	return (
 		<div className={iconheaderStyles.container}>
-			<img style={} className={iconheaderStyles.icon} src={src} />
+			<img style={iconColors.get(color)} className={iconheaderStyles.icon} src={src} />
 			<h5 className={textStyles.get(type)}></h5>
 		</div>
 	);
