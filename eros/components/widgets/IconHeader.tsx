69f2dc6b66beda7b1f9d9a643cafcdbd2e6ec207
Ashton <65512990+ashtonland@users.noreply.github.com>
2022-08-10 20:46:14 -0400

    Header text parameter

--- eros/components/widgets/IconHeader.tsx
+++ eros/components/widgets/IconHeader.tsx
@@ -4,11 +4,12 @@ interface IconHeaderProps {
 	type: "widget";
 	src: string;
 	color: "purple" | "pink-yellow" | "gold" | "red" | "pinky-red" | "blue" | "green";
+	text: string;
 }
 
 const purple = { background: "linear-gradient(120.25deg, #7594FF 11.25%, #685CEF 88.64%)", boxShadow: "0px 0.375em 0.938em rgba(104, 92, 239, 0.3)" };
 
-const IconHeader = ({ type, src, color }: IconHeaderProps) => {
+const IconHeader = ({ type, src, color, text }: IconHeaderProps) => {
 	// map type to header text styling
 	const textStyles = new Map<string, any>();
 	textStyles.set("widget", iconheaderStyles.widget);
@@ -19,7 +20,7 @@ const IconHeader = ({ type, src, color }: IconHeaderProps) => {
 	return (
 		<div className={iconheaderStyles.container}>
 			<img style={iconColors.get(color)} className={iconheaderStyles.icon} src={src} />
-			<h5 className={textStyles.get(type)}></h5>
+			<h5 className={textStyles.get(type)}>{text}</h5>
 		</div>
 	);
 };
--- eros/components/widgets/People.tsx
+++ eros/components/widgets/People.tsx
@@ -4,7 +4,7 @@ import IconHeader from "./IconHeader";
 const PeopleWidget = ({ count }: { count: number }) => {
 	return (
 		<div className={widgetStyles.peoplewidget}>
-			<IconHeader type="widget" src="/resources/person.svg" color="purple" />
+			<IconHeader type="widget" src="/resources/person.svg" color="purple" text="People" />
 		</div>
 	);
 };
