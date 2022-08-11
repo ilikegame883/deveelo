7696bcd364abf0e4a72c569c5c815fcad780597f
Ashton <65512990+ashtonland@users.noreply.github.com>
2022-08-10 22:13:47 -0400

    Widget user card component

---
+++ eros/components/widgets/UserCard.tsx
@@ -0,0 +1,18 @@
+import cardStyles from "../../styles/micro/widgetcards.module.css";
+import NameGroup from "../micro/NameGroup";
+
+interface UserCardProps {
+	account: any;
+	profile: any;
+	status: string;
+}
+
+const UserCard = ({ account, profile, status }: UserCardProps) => {
+	return (
+		<div className={cardStyles.card}>
+			<NameGroup username={account.username} size={5} badges={profile.badges} showBadges={true} outline={true} disableSpacer={true} />
+		</div>
+	);
+};
+
+export default UserCard;
