479963a3d1ee2744e55a846651beb8c31b666895
Ashton <65512990+ashtonland@users.noreply.github.com>
2022-08-11 09:36:17 -0400

    Return input type after sort

--- eros/hooks/sortUsers.ts
+++ eros/hooks/sortUsers.ts
@@ -1,19 +1,21 @@
-import { SortUserType } from "../lib/userTypes";
+//used only during development--for intellisense--otherwise the sorting will return
+//this type, not the input, therefore, many fields will be lost after sorting
+//import { SortUserType } from "../lib/userTypes";
 
 //closer to index 0 should be placed higher
 const ranking = ["online", "idle", "dnd", "offline"];
 
-export const sortByStatus = (users: SortUserType[]) => {
+export const sortByStatus = (users: any[]) => {
 	//if be is a lower index, move b up
 	return users.sort((a, b) => (ranking.indexOf(a.status) > ranking.indexOf(b.status) ? 1 : -1));
 };
 
-export const sortById = (users: SortUserType[]) => {
+export const sortById = (users: any[]) => {
 	//smaller ids are favored: this does not mean anything though, ids are random
 	return users.sort((a, b) => (parseInt(a._id) > parseInt(b._id) ? 1 : -1));
 };
 
-export const sortByStatusAndId = (users: SortUserType[]) => {
+export const sortByStatusAndId = (users: any[]) => {
 	//if the status is of a lower index, move it up (i.e. online moves above idle)
 	//if the statuses are equal, then sort by user id
 	//if the status is of a higher index, it is properly ranked below
