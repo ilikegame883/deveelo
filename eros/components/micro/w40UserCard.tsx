5406268739ef5439fb7f026a99ea6e827f51c9d4
Ashton <65512990+ashtonland@users.noreply.github.com>
2022-08-15 21:02:25 -0400

    Reorganize card w/o button overlaps

--- eros/components/micro/w40UserCard.tsx
+++ eros/components/micro/w40UserCard.tsx
@@ -1,9 +1,11 @@
-import { updateSidebar } from "../../hooks/socialhooks";
-import { SearchAccountType, SearchProfileType } from "../../lib/userTypes";
-import w40styles from "../../styles/micro/w40.module.css";
 import IconButton from "./IconButton";
 import NameGroup from "./NameGroup";
 import ProfilePicture from "./ProfilePicture";
+import w40styles from "../../styles/micro/w40.module.css";
+import cardStyles from "../../styles/micro/widgetcards.module.css";
+
+import { updateSidebar } from "../../hooks/socialhooks";
+import { SearchAccountType, SearchProfileType } from "../../lib/userTypes";
 
 interface CardProps {
 	key: string;
@@ -23,15 +25,17 @@ const W40UserCard = ({ key, account, profile, status }: CardProps) => {
 		updateSidebar(tag);
 	};
 	return (
-		<div className={w40styles.cardwrapper} onClick={() => changeSidebar(account.tag)}>
+		<div className={w40styles.cardwrapper}>
 			<div className="fitfillcenter">
-				<div className={w40styles.namepicWrapper}>
-					<ProfilePicture size="w32" source={profile.pictureUrl} status={status} />
-					<div className={w40styles.nameWrapper}>
-						<NameGroup username={account.username} size={4} badges={profile.badges} disableSpacer={true} showBadges={true} outline={true} />
+				<div className={cardStyles.linkgroup} onClick={() => changeSidebar(account.tag)}>
+					<div className={w40styles.namepicWrapper}>
+						<ProfilePicture size="w32" source={profile.pictureUrl} status={status} />
+						<div className={w40styles.nameWrapper}>
+							<NameGroup username={account.username} size={4} badges={profile.badges} disableSpacer={true} showBadges={true} outline={true} />
+						</div>
 					</div>
 				</div>
-				<div className="fillfillcenterright">
+				<div className={w40styles.buttonContainer}>
 					<IconButton src="/resources/followbell.svg" activesrc="/resources/onbell.svg" width="2rem" height="2rem" paddingLR={0.375} paddingTB={0.375} />
 				</div>
 			</div>
