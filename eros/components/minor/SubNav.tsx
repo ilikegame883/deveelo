423befd6f8b2249ba17b54d919b03504a5a4f90c
Ashton <65512990+ashtonland@users.noreply.github.com>
2022-08-17 20:51:59 -0400

    Button list

--- eros/components/minor/SubNav.tsx
+++ eros/components/minor/SubNav.tsx
@@ -1,9 +1,14 @@
 import subnavStyles from "../../styles/minor/subnav.module.css";
+import IconButton from "../micro/IconButton";
 
 const SubNav = () => {
 	return (
 		<div className={subnavStyles.fadeWrapper}>
-			<div className={subnavStyles.notch}></div>
+			<div className={subnavStyles.notch}>
+				<div className={subnavStyles.buttonList}>
+					<IconButton src="resources/home.svg" width="4.219em" height="4.219em" paddingLR={0.703} paddingTB={0.703} />
+				</div>
+			</div>
 		</div>
 	);
 };
