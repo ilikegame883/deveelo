ab4399c969306a5ab97b25106fa20a53dfb7d5fa
Ashton <65512990+ashtonland@users.noreply.github.com>
2022-08-17 21:09:03 -0400

    Fix icon not found in nested pages

--- eros/components/minor/SubNav.tsx
+++ eros/components/minor/SubNav.tsx
@@ -7,7 +7,7 @@ const SubNav = () => {
 		<div className={subnavStyles.fadeWrapper}>
 			<div className={subnavStyles.notch}>
 				<div className={subnavStyles.buttonList}>
-					<SubnavButton src="resources/home.svg" path="/" key="0" color="linear-gradient(135deg, #FFD74A 0%, #FFB129 100%)" width="4.219em" height="4.219em" padding={0.703} />
+					<SubnavButton src="/resources/home.svg" path="/" key="0" color="linear-gradient(135deg, #FFD74A 0%, #FFB129 100%)" width="4.219em" height="4.219em" padding={0.703} />
 					{/* <IconButton src="resources/home.svg" width="4.219em" height="4.219em" paddingLR={0.703} paddingTB={0.703} /> */}
 				</div>
 			</div>
