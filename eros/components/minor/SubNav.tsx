c5ce3ca5e0717d0fcea1ab7cc8497bdd9fadf13d
Ashton <65512990+ashtonland@users.noreply.github.com>
2022-08-18 11:56:27 -0400

    Subnav button sizing

--- eros/components/minor/SubNav.tsx
+++ eros/components/minor/SubNav.tsx
@@ -7,8 +7,10 @@ const SubNav = () => {
 		<div className={subnavStyles.fadeWrapper}>
 			<div className={subnavStyles.notch}>
 				<div className={subnavStyles.buttonList}>
-					<SubnavButton src="/resources/home.svg" path="/" key="0" color="linear-gradient(135deg, #FFD74A 0%, #FFB129 100%)" width="4.219em" height="4.219em" padding={0.703} />
-					{/* <IconButton src="resources/home.svg" width="4.219em" height="4.219em" paddingLR={0.703} paddingTB={0.703} /> */}
+					<SubnavButton src="/resources/home.svg" path="/" key="0" color="linear-gradient(135deg, #FFD74A 0%, #FFB129 100%)" />
+					<SubnavButton src="/resources/blog.svg" path="/devlogs" key="1" color="linear-gradient(135deg, #FFD74A 0%, #FFB129 100%)" />
+					<SubnavButton src="/resources/groups.svg" path="/groups" key="2" color="linear-gradient(135deg, #FFD74A 0%, #FFB129 100%)" />
+					<SubnavButton src="/resources/betas.svg" path="/betas" key="3" color="linear-gradient(135deg, #FFD74A 0%, #FFB129 100%)" />
 				</div>
 			</div>
 		</div>
