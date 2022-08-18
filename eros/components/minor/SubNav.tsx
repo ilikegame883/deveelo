681a97392785115fbbb68e2db8f7b72dfbf63538
Ashton <65512990+ashtonland@users.noreply.github.com>
2022-08-17 20:21:39 -0400

    Subnav framing

---
+++ eros/components/minor/SubNav.tsx
@@ -0,0 +1,11 @@
+import subnavStyles from "../../styles/minor/subnav.module.css";
+
+const SubNav = () => {
+	return (
+		<div className={subnavStyles.fadeWrapper}>
+			<div className={subnavStyles.notch}></div>
+		</div>
+	);
+};
+
+export default SubNav;
