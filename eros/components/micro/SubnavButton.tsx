86c2a99d23d9e19d697ccc5d73751ded68ffc45d
Ashton <65512990+ashtonland@users.noreply.github.com>
2022-08-19 18:44:44 -0400

    Document system

--- eros/components/micro/SubnavButton.tsx
+++ eros/components/micro/SubnavButton.tsx
@@ -13,13 +13,13 @@ const SubnavButton = ({ key, src, path, color }: SNB_Props) => {
 
 	//use color for this button if its click path matches the current (we are on its page)
 	const validUrls = ["/", "/devlogs", "/groups", "/home"];
+	//check if the current route starts the same as this button' link to path; also
+	//check if the first /blahblah in the current route is in the valid site pages urls array
 	const active = router.pathname.substring(0, 4) === path.substring(0, 4) && validUrls.includes(router.pathname.substring(0, path.length));
-	// if (validUrls.includes(router.pathname.substring(0, path.length)))
 
 	const iconStyle = () => ({
 		width: "100%",
 		height: "100%",
-		// padding: `${padding}rem`,
 	});
 
 	return (
