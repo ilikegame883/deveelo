bb4fe99959de314dbd0295aa3f9828bc5dd79ff4
Ashton <65512990+ashtonland@users.noreply.github.com>
2022-10-24 18:49:29 -0400

    Update post area event hook

--- eros/hooks/socialhooks.ts
+++ eros/hooks/socialhooks.ts
@@ -1,3 +1,5 @@
+//refresh sidebar after profile editting to immediately show
+//the new banner, pfp, or text-based account details w/o page reload
 export const updateSidebar = (tag: string) => {
 	const side = document.getElementById("sidebar");
 	if (side) {
@@ -5,5 +7,16 @@ export const updateSidebar = (tag: string) => {
 		console.log("dispatched");
 
 		side.dispatchEvent(updateEvent);
+	}
+};
+
+//clear fields & switch to share after posting
+export const updatePostArea = (newscreen: "afterpost" | "reset", imageName: string) => {
+	const postarea = document.getElementById("postarea");
+	if (postarea) {
+		const updateEvent = new CustomEvent("updatePostArea", { detail: newscreen + "|" + imageName });
+		console.log("dispatched");
+
+		postarea.dispatchEvent(updateEvent);
 	}
 };
