fe2c4eae6a71922719a7c3edea3fb4f9b49e3ac7
Ashton <65512990+ashtonland@users.noreply.github.com>
2022-11-20 20:17:22 -0500

    Load post images

--- eros/components/posts/PostCard.tsx
+++ eros/components/posts/PostCard.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+import { postLoader } from "../../hooks/loaders";
 import { PostType } from "../../lib/postTypes";
 import styles from "../../styles/posts/postcard.module.css";
 
@@ -8,7 +10,13 @@ interface PC_Props {
 const PostCard = ({ post }: PC_Props) => {
 	const { body, imageUrls, user_id, createdAt, likes, comments } = post;
 
-	return <div>PostCard</div>;
+	return (
+		<div className={styles.card}>
+			<div className={styles.imageWrapper}>
+				<Image loader={postLoader} src={imageUrls[0]} className={styles.image} layout="fill" priority={true} objectFit="cover" />
+			</div>
+		</div>
+	);
 };
 
 export default PostCard;
